3539e08f663df4468ca02f6aa1116b422a4fe7be
TrevorKS1 <trevorksansalone@gmail.com>
2021-10-20 00:19:15 -0500

    setting up our variable sorts declaring and prepping to use in our functions.

---
+++ src/store/index.js
@@ -0,0 +1,8 @@
+// Creating actions to start with! 
+const SORT_BY_ALPHABET = "SORT_BY_ALPHABET"; 
+const SORT_BY_PRICE = "SORT_BY_PRICE";
+const LOAD_DATA = "LOAD_DATA";
+const FILTER_BY_PRICE = "FILTER_BY_PRICE"; 
+// Sorting variables 
+
+// Then exporting those functions to return an object. Passing everything within action to reducers.
